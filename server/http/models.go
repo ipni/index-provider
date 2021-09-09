52c67d0e06520557e7dd5e705c7bb6de22e0b7ca
Masih H. Derkani <m@derkani.org>
2021-09-09 16:49:51 +0200

    Implement admin server handler for importing CAR files
    Implement handler for POST requests to `admin/import/car` endpoint to
    request import of a CAR file from a given path with optional key and
    metadata.

--- server/http/models.go
+++ server/http/models.go
@@ -1,6 +1,26 @@
 package adminserver
 
+import "github.com/ipfs/go-cid"
+
 // ConnectReq request
 type ConnectReq struct {
 	Maddr string
+}
+
+// ImportCarReq represents a request for importing a CAR file.
+type ImportCarReq struct {
+	// The path to the CAR file
+	Path string `json:"path"`
+	// The optional lookup key associated to the CAR. If not provided, one will be generated.
+	Key []byte `json:"key"`
+	// The optional metadata.
+	Metadata []byte `json:"metadata"`
+}
+
+// ImportCarRes represents the response to an ImportCarReq.
+type ImportCarRes struct {
+	// The lookup Key associated to the imported CAR.
+	Key []byte `json:"key"`
+	// The CID of the advertisement generated as a result of import.
+	AdvId cid.Cid `json:"adv_id"`
 }
