8aa3ae802772c60fd37ed546ef3e50eae6ed772a
Masih H. Derkani <m@derkani.org>
2021-10-19 16:08:34 +0100

    Refactor `Callback` API due to iteration determinism changes
    Address TODOs left in code to refactor the `Callback` API now that all
    multihashes are read and sorted when CAR multihash iterator is
    constructed.
    
    As a result we do not need `context` in CAR mh iterator implementation
    of `Callback` anymore since there is no gorutine in the background to
    manage the lifecycle of. We do however keep the `context` in signature
    of `Callback` for user whom wish to use an alternative implementation of
    `Callback`.
    
    Fail fast when `ForEach` traversal of multihashes done in constructor
    of CAR multihash iterator returns error. This makes a simpler error
    handling logic and makes sense to fail fast since the `Callback`
    function already returns error.
    
    Relates to #62

--- index_mh_iter.go
+++ index_mh_iter.go
@@ -1,7 +1,6 @@
 package provider
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"sort"
@@ -14,7 +13,6 @@ var _ MultihashIterator = (*indexMhIterator)(nil)
 
 type indexMhIterator struct {
 	steps []iteratorStep
-	err   error
 
 	curStep    int
 	lastOffset uint64
@@ -27,34 +25,25 @@ type iteratorStep struct {
 
 // CarMultihashIterator constructs a new MultihashIterator from a CAR index.
 //
-// A background goroutine is started to supply multihashes via Next method calls.
-// Its lifecycle is controlled by the given context.
-// If the context is canceled before Next reaches io.EOF,
-// then the following Next call will return the context's error.
-func CarMultihashIterator(ctx context.Context, idx carindex.IterableIndex) MultihashIterator {
-	// TODO(mvdan): if we stick with this "upfront range and sort" approach,
-	// we probably need to rethink this API a bit.
-	// At the very least, to remove the context.
-
+// This iterator supplies multihashes in deterministic order of their corresponding CAR offset.
+// The order is maintained consistently regardless of the underlying IterableIndex implementation.
+func CarMultihashIterator(idx carindex.IterableIndex) (MultihashIterator, error) {
 	var steps []iteratorStep
-	err := idx.ForEach(func(mh multihash.Multihash, offset uint64) error {
+	if err := idx.ForEach(func(mh multihash.Multihash, offset uint64) error {
 		steps = append(steps, iteratorStep{mh, offset})
 		return nil
-	})
+	}); err != nil {
+		return nil, err
+	}
 	sort.Slice(steps, func(i, j int) bool {
 		return steps[i].offset < steps[j].offset
 	})
-	return &indexMhIterator{steps: steps, err: err}
+	return &indexMhIterator{steps: steps}, nil
 }
 
 func (i *indexMhIterator) Next() (multihash.Multihash, error) {
 	// We have reached the end of stream.
-	// There might also be an error available, which we should return.
 	if i.curStep >= len(i.steps) {
-		// Check if there is a error first, since returning error must take precedence.
-		if i.err != nil {
-			return nil, i.err
-		}
 		return nil, io.EOF
 	}
 	step := i.steps[i.curStep]
