4ccbd15380a0b52ae6b5c0a34be455c6c9e585ce
gammazero <gammazero@users.noreply.github.com>
2022-05-10 14:33:59 -0700

    Smaller default MultihashIterator implementation
    The default MultihashIterator implementation does not need to keep the offsets if a check is done for duplicates when the iterator is created. This reduces memory allocated for each iterator instance, by n uint64 values, at the cost of one O(n) iteration of the sorted offsets to check for duplicates.
    
    The default implementation is also exported so that it can be used in other packages that only need a simple multihash iterator.

--- index_mh_iter.go
+++ index_mh_iter.go
@@ -9,13 +9,13 @@ import (
 	"github.com/multiformats/go-multihash"
 )
 
-var _ MultihashIterator = (*indexMhIterator)(nil)
+var _ MultihashIterator = (*MhIterator)(nil)
 
-type indexMhIterator struct {
-	steps []iteratorStep
-
-	curStep    int
-	lastOffset uint64
+// MhIterator is a simple MultihashIterator implementation that iterates a
+// slice of multihash.Multihash.
+type MhIterator struct {
+	mhs []multihash.Multihash
+	pos int
 }
 
 type iteratorStep struct {
@@ -25,9 +25,31 @@ type iteratorStep struct {
 
 // CarMultihashIterator constructs a new MultihashIterator from a CAR index.
 //
-// This iterator supplies multihashes in deterministic order of their corresponding CAR offset.
-// The order is maintained consistently regardless of the underlying IterableIndex implementation.
+// This iterator supplies multihashes in deterministic order of their
+// corresponding CAR offset. The order is maintained consistently regardless of
+// the underlying IterableIndex implementation.
 func CarMultihashIterator(idx carindex.IterableIndex) (MultihashIterator, error) {
+	mhs, err := carIndexToMultihashes(idx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MhIterator{mhs: mhs}, nil
+}
+
+// Next implements the MultihashIterator interface.
+func (it *MhIterator) Next() (multihash.Multihash, error) {
+	if it.pos >= len(it.mhs) {
+		return nil, io.EOF
+	}
+	mh := it.mhs[it.pos]
+	it.pos++
+	return mh, nil
+}
+
+// carIndexToMultihashes converts a CAR index to an ordered slice of
+// Multihash. Returns error if duplicate offsets detected.
+func carIndexToMultihashes(idx carindex.IterableIndex) ([]multihash.Multihash, error) {
 	var steps []iteratorStep
 	if err := idx.ForEach(func(mh multihash.Multihash, offset uint64) error {
 		steps = append(steps, iteratorStep{mh, offset})
@@ -38,22 +60,15 @@ func CarMultihashIterator(idx carindex.IterableIndex) (MultihashIterator, error)
 	sort.Slice(steps, func(i, j int) bool {
 		return steps[i].offset < steps[j].offset
 	})
-	return &indexMhIterator{steps: steps}, nil
-}
 
-func (i *indexMhIterator) Next() (multihash.Multihash, error) {
-	// We have reached the end of stream.
-	if i.curStep >= len(i.steps) {
-		return nil, io.EOF
+	var lastOffset uint64
+	mhs := make([]multihash.Multihash, len(steps))
+	for i := range steps {
+		if steps[i].offset == lastOffset {
+			return nil, fmt.Errorf("car multihash iterator has duplicate offset %d", steps[i].offset)
+		}
+		mhs[i] = steps[i].mh
 	}
-	step := i.steps[i.curStep]
-	i.curStep++
-	if step.offset < i.lastOffset {
-		return nil, fmt.Errorf("car multihash iterator out of order: %d then %d", i.lastOffset, step.offset)
-	}
-	if step.offset == i.lastOffset {
-		return nil, fmt.Errorf("car multihash iterator has duplicate offset %d", step.offset)
-	}
-	i.lastOffset = step.offset
-	return step.mh, nil
+
+	return mhs, nil
 }
